8be71790da1baf463a0f2e96509cb81de4802247
Jeremy <jbarquero@cog.cr>
2020-12-14 16:48:27 -0600

    Update new

--- back-end/app.js
+++ back-end/app.js
@@ -95,7 +95,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/tico-eats/index.html'));
 });
 
 // Start Server
